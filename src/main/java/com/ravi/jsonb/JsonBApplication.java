d4744fc123da7c67c181a4cab385a2e5bcf65582
raveendrabikkina <braveendra@gmail.com>
2019-08-18 15:54:22 +1000

    New

---
+++ src/main/java/com/ravi/jsonb/JsonBApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.jsonb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonBApplication.class, args);
+    }
+
+}
